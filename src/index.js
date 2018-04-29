ce088b235a2c8bc1031256fc468db2f8f244f218
Joy <joyterra@gmail.com>
2018-04-29 15:52:15 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import App from './App';
+import AddItem from './components/AddItem';
+
+ReactDOM.render(
+  <Router>
+      <div>
+        <Route exact path='/' component={App} />
+      </div>
+  </Router>,
+  document.getElementById('root')
+);
